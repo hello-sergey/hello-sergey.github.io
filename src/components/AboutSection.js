9090e0a902e1f41d0bd6c1a9336f42d7df440092
Sergey Batechko <sergey.batechko@dataart.com>
2023-03-10 00:19:30 +0200

    fixed about text

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -11,7 +11,7 @@ const AboutSection = forwardRef((props, ref) => {
                         QA Automation engineer with experience in data parsing and web development.
                         2 years experience in QAA. Graduated from Odessa National Polytechnic University, Institute of
                         Information
-                        Security, Radio Electronics and Telecommunications with masters degree in Cybersecurity.
+                        Security, Radio Electronics and Telecommunications with Master's degree in Cybersecurity.
                     </p>
                 </div>
             </div>
